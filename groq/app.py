61897d5482784c94980a71d66e923057e42b0312
Paritosh Shukla <143748029+paritosh-Shukla24@users.noreply.github.com>
2024-06-30 19:03:11 +0530

    Add files via upload

--- groq/app.py
+++ groq/app.py
@@ -18,8 +18,8 @@ groq_api_key=os.environ['groq_api_key']
 gemini_api_key=os.environ['google_api_key']
 if "vector" not in st.session_state:
     st.session_state.embeddings=GoogleGenerativeAIEmbeddings(gemini_api_key=gemini_api_key,model="models/embedding-001")
-    # st.session_state.loader=WebBaseLoader("https://docs.smith.langchain.com/")
-    st.session_state.docs=WebBaseLoader("https://docs.smith.langchain.com/").load()
+     
+    st.session_state.docs=WebBaseLoader("https://brainlox.com/").load()
     # st.session_state.docs=st.session_state.loader.load()
     st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
     st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
